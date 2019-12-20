56cd1df0fa441298f4fb99bd203d87eeaa510e24
임성훈 <sasumpi123@naver.com>
2019-12-20 15:49:57 +0900

    JavaScript getElement

--- UI03_JavaScript/WebContent/js/test.js
+++ UI03_JavaScript/WebContent/js/test.js
@@ -1,7 +1,7 @@
 function test() {
-	window.alert("외부 작성 방식");
+    window.alert("외부 작성 방식");
 }
 
-window.onload=function(){
-	alert("윈도우 로드 됨");
+window.onload = function() {
+    alert("윈도우 로드 됨");
 }
